5b2e5a08aa11dc397c3f678ed0286bd85249efb5
Leigh Hall <lhall@cafex.com>
2021-12-21 13:59:47 +0000

    layout tweaks

--- molkky.js
+++ molkky.js
@@ -1,13 +1,11 @@
 // ios vh workaround
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
-console.error(`+++ initial ${vh}`);
 
 window.addEventListener('resize', () => {
     // We execute the same script as before
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-    console.error(`+++ resize ${vh}`);
 });
 
 var slideIndex = 1;
